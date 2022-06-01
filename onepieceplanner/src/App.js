d97ce52c9782cc531270d3dfc782a6ec0c08c37e
AP <arjavprasad@gmail.com>
2022-06-01 11:59:11 -0500

    set up compents and redux reducers

--- onepieceplanner/src/App.js
+++ onepieceplanner/src/App.js
@@ -1,9 +1,29 @@
-function App() {
-	return (
-		<div className="App">
-			<h1>One Piece Planner</h1>
-		</div>
-	);
+import React, {Component} from 'react';
+import {BrowserRouter, Switch, Route, Routes} from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard';
+import Navbar from './components/layout/Navbar';
+import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
+
+class App extends Component {
+	render() {
+		return (
+			<BrowserRouter>
+				<div className="App">
+					<Navbar />
+					<Routes>
+						<Route exact path="/" element={<Dashboard />} />
+						<Route path="/project/:id" element={<ProjectDetails />} />
+						<Route path="/signin" element={<SignIn />} />
+						<Route path="/signUp" element={<SignUp />} />
+						<Route path="/create" element={<CreateProject />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
---
+++ onepieceplanner/src/components/layout/SignedInLinks.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const SignedInLinks = () => {
+	return (
+		<div>
+			<ul className="right">
+				<li>
+					<NavLink to="/create">New Project</NavLink>
+				</li>
+				<li>
+					<NavLink to="/">Log Out</NavLink>
+				</li>
+				<li>
+					<NavLink to="/" className="btn btn-floating pink lighten-1">
+						AP
+					</NavLink>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default SignedInLinks;
