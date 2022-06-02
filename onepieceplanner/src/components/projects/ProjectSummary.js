c76194a5b48078f559c3ba4b00412171bc665dd8
AP <arjavprasad@gmail.com>
2022-06-01 22:45:11 -0500

    added firebase/firestorm config stuff

--- onepieceplanner/src/components/projects/ProjectSummary.js
+++ onepieceplanner/src/components/projects/ProjectSummary.js
@@ -1,11 +1,11 @@
 import react from 'react';
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
 	return (
 		<div className="card z-depth-0 project-summary">
 			<div className="card-content grey-text text-darken-3">
-				<span className="card-title ">Project title</span>
-				<p>Posted by The Net Ninja</p>
+				<span className="card-title ">{project.title}</span>
+				<p>Posted by Monkey D. Luffy</p>
 				<p className="grey-text">3rd September, 2am</p>
 			</div>
 		</div>
--- onepieceplanner/src/index.js
+++ onepieceplanner/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
